fbdf3f00d250c12940b072408a9363b5533a6b1b
Cassiano Castro <development.castro@gmail.com>
2024-02-29 14:33:04 -0300

    Changed test case

--- index.php
+++ index.php
@@ -3,10 +3,8 @@ ini_set("display_errors", true);
 ini_set("display_startup_errors", true);
 error_reporting(E_ALL);
 
-setlocale(LC_ALL, "", "pt_BR.utf8");
+setlocale(LC_ALL, "");
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo "<pre>";
-$connection = (new Creational\App())->getConnection();
-(new Helpers\Tester())->test($connection);
+(new Tests\Tester())->canGetAllUsers();
